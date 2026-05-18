41b97ddeb9e52fa1473f8e3b0506eae54ff7da9c
nekomaido <meowbotz.lll@gmail.com>
2026-05-18 16:59:28 +0800

    test: demonstrate ci failure

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
 
   app.get('/', async () => {
     return {
-      message: 'CI/CD Lab Fastify app is running',
+      message: 'Broken CI demo message',
       version: process.env.APP_VERSION || 'dev'
     };
   });
